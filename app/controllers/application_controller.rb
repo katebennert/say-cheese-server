9f018812bd423083ca4be1ef0594e03dd0214fd5
Kate Bennert <kate.bennert@gmail.com>
2023-03-07 18:31:19 -0500

    Fix routes to include freelancers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
   get '/jobs' do
     jobs = Job.all 
-    jobs.to_json
+    jobs.to_json(include: :freelancers)
   end
 
   get '/jobs/:id' do
@@ -25,6 +25,30 @@ class ApplicationController < Sinatra::Base
   get '/freelancers/:id' do
     freelancer = Freelancer.find(params[:id])
     freelancer.to_json
+  end
+
+  patch '/freelancers/:id' do 
+    freelancer = Freelancer.find(params[:id])
+    freelancer.update(
+      is_available: params[:is_available],
+      job_id: params[:job_id]
+      )
+    freelancer.to_json
+  end
+
+  patch '/jobs/:id' do 
+    job = Job.find(params[:id])
+    job.update(
+      is_full: params[:is_full],
+      freelancers_needed: params[:freelancers_needed]
+      )
+    job.to_json(include: :freelancers)
+  end
+
+  delete '/jobs/:id' do
+    job = Job.find(params[:id])
+    job.destroy
+    job.to_json(include: :freelancers)
   end
   
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Freelancer.create([
     email: "cannoli@gmail.com",
     freelancer_type: "Photographer",
     image_url: "https://photos.google.com/album/AF1QipOncioxKp5h4mxMTRBAZDV7v2931U5ew-xxbY47/photo/AF1QipMkvl72gBTj0VvC02vwS1pYs6mJ4t1A-o55V3hv",
-    is_available: true
+    is_available: false
   },
   {
     name: "Kim K.",
@@ -40,15 +40,37 @@ Freelancer.create([
 
 Job.create([
     {
-        name: "Marketing Photoshoot for Menu Items",
-        company: "Big Chicken Company",
-        company_logo: "",
-        start_date: 2023-03-15,
-        end_date: 2023-03-16,
-        description: "Big Chicken Company is conducting our yearly shoot for our new menu items. We are looking for a photographer, producer and food stylist to create images and videos to use in out online promotinal materials.",
-        freelancers_needed: 4,
-        is_full: false,
-        is_open: true
+      name: "Marketing Photoshoot for Menu Items",
+      company: "Big Chicken Company",
+      company_logo: "",
+      start_date: 2023-03-15,
+      end_date: 2023-03-16,
+      description: "Big Chicken Company is conducting our yearly shoot for our new menu items. We are looking for a photographer, producer and food stylist to create images and videos to use in out online promotinal materials.",
+      freelancers_needed: 4,
+      is_full: false,
+      is_open: true
+    },
+    {
+      name: "TV Commercial for New Sandwich",
+      company: "Big Burgers",
+      company_logo: "",
+      start_date: 2023-03-15,
+      end_date: 2023-03-16,
+      description: "We're shooting a commercial for our really yummy and brand new sandwich.",
+      freelancers_needed: 2,
+      is_full: false,
+      is_open: true
+    },
+    {
+      name: "Full Branding Package for our new CBD Chili Oil",
+      company: "Peper Hemp",
+      company_logo: "",
+      start_date: 2023-03-15,
+      end_date: 2023-03-16,
+      description: "We have a brand new awesome product! We know this can really take off and we need several team members to start ASAP!",
+      freelancers_needed: 10,
+      is_full: false,
+      is_open: true
     }
 ])
 
